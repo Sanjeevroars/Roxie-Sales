f98e250f35332d3aa8f602d68cbc754f7b345a41
EkMishra <ekagra.mishra@hotmail.com>
2025-01-25 13:21:15 +0530

    patch date

--- backend/app.js
+++ backend/app.js
@@ -62,7 +62,7 @@ app.post("/api/end_conversation", async (req, res) => {
   const { user_info } = req.body;
   // console.log(user_info);
 
-  const { interested_model, date, ...otherData } = user_info;
+  const { interested_model, ...otherData } = user_info;
   // console.log(interested_model)
   const client = await Client.findOne({ model: interested_model });
   // console.log(client);
@@ -94,6 +94,28 @@ const getTraffic = async () => {
   ]);
 };
 
+const  getEnquiryForEachProduct = async  ()=>{
+  return await Enquiry.aggregate([
+    {
+      $lookup:{
+        from:"clientmodels",
+        localField:"interested_model",
+        foreignField:"_id",
+        as:"productDetails",
+      },
+    },
+    {
+      $unwind:"$productDetails",
+    },
+    {
+      $group: {
+        _id: "$productDetails.name", // Group by product name
+        enquiriesCount: { $sum: 1 }, // Count the number of enquiries
+      },
+    },
+  ])
+}
+
 app.get("/traffic-each-day", async (req, res) => {
   const trafficData = await getTraffic();
   console.log(trafficData);
@@ -101,6 +123,10 @@ app.get("/traffic-each-day", async (req, res) => {
     data: trafficData,
   });
 });
+app.get("/enquiry-per-product",async (req,res)=>{
+  const enquiryProduct = await getEnquiryForEachProduct();
+
+})
 app.listen(3000, () => {
   console.log(`App running on Port ${PORT}`);
 });
