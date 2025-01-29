70bbc9a412a29b1b716d29187c379df8386a2ec1
anuragrege <anuragrege@gmail.com>
2025-01-30 05:28:20 +0530

    add dashboard button toyota

--- Toyota/components/Navbar.tsx
+++ Toyota/components/Navbar.tsx
@@ -1,30 +1,38 @@
+"use client";
+
 import Link from "next/link";
-import Image from "next/image";
 
-import CustomButton from "./CustomButton";
+const NavBar = () => {
+  const handleRedirect = () => {
+    window.location.href = "http://localhost:3009";
+  };
 
-const NavBar = () => (
-  <header className="w-full  absolute z-10">
-    <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
-      <Link href="/" className="flex justify-center items-center">
-        {/* <Image
-                    src='/logo.svg'
-                    alt='logo'
-                    width={118}
-                    height={18}
-                    className='object-contain'
-                /> */}
-      </Link>
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+        <Link href="/" className="flex justify-center items-center">
+          {/* Your logo here, you can uncomment if needed */}
+          {/* <Image
+            src='/logo.svg'
+            alt='logo'
+            width={118}
+            height={18}
+            className='object-contain'
+          /> */}
+        </Link>
 
-      <button
-        title="Welcome!"
-        type="button"
-        className="text-[#2751E9] rounded-full bg-white min-w-[130px] py-2 px-4 text-center"
-      >
-        Welcome!
-      </button>
-    </nav>
-  </header>
-);
+        {/* Dashboard Button */}
+        <button
+          title="Dashboard"
+          type="button"
+          className="text-[#2751E9] rounded-full bg-white min-w-[130px] py-2 px-4 text-center"
+          onClick={handleRedirect}
+        >
+          Dashboard
+        </button>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
